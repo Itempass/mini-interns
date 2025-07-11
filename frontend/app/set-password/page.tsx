2950ca353a7fac0e5a9a4c0564cb18ab76636a3a
ArthurDevel <arthur@itempass.com>
2025-07-11 17:46:57 +0200

    - added warning message that password cannot be recovered

--- frontend/app/set-password/page.tsx
+++ frontend/app/set-password/page.tsx
@@ -83,6 +83,21 @@ export default function SetPasswordPage() {
             />
           </div>
 
+          <div className="rounded-md bg-yellow-50 p-4">
+            <div className="flex">
+              <div className="flex-shrink-0">
+                <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM10 13a1 1 0 110-2 1 1 0 010 2zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+              </div>
+              <div className="ml-3">
+                <p className="text-sm text-yellow-800">
+                  Please write this password down. It <span className="font-bold">cannot</span> be reset or recovered if you lose it.
+                </p>
+              </div>
+            </div>
+          </div>
+
           {error && <p className="text-sm text-center text-red-600">{error}</p>}
 
           <div>
